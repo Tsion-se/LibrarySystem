a94d0aa28a70974922cebeecc1396d89a3f0b5d2
kiyu16 <tsionasratkiya@gmail.com>
2025-12-24 12:40:13 +0300

    Initial commit - java code generated from UML

---
+++ Loan.java
@@ -0,0 +1,10 @@
+public class Loan {
+    public void borrowBook() {
+        System.out.println("Borrowed");
+    }
+
+    public void returnBook() {
+        System.out.println("Returned");
+    }
+
+}
