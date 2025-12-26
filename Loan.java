42062f871d2e37d4b36d9e72ad487c98421ab046
eyutaa <eyerusalemberihun333@gmail.com>
2025-12-26 10:04:29 +0300

    Isak: implement loan feature

--- Loan.java
+++ Loan.java
@@ -3,7 +3,7 @@ public class Loan {
         System.out.println("Borrowed");
     }
 
-    public void returnBook() {
+    public void returnBook() { // validating the void method by eyerusalem
         System.out.println("Returned");
         system.out.println("loan record updated");
     }
